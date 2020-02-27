02d9adaab14925c3097d5cb9d66c424e060f8b53
Carl Bradbury <carl@gnarlydog.co.uk>
2020-02-27 00:00:08 +0000

    Added transactions

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,12 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const AddTransaction = () => {
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 1000000),
+      text,
+      amount: +amount
+    };
+
+    addTransaction(newTransaction);
+  };
   const [text, setText] = useState('');
   const [amount, setAmount] = useState('');
   return (
     <>
       <h2>Add Transaction</h2>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className='form-control'>
           <label htmlFor='text'>Text</label>
           <input
@@ -16,14 +30,12 @@ export const AddTransaction = () => {
           />
         </div>
         <div className='form-control'>
-          <label
-            htmlFor='amount'
+          <label htmlFor='amount'>Amount</label>
+          <input
+            type='number'
             value={amount}
             onChange={e => setAmount(e.target.value)}
-          >
-            Amount
-          </label>
-          <input type='number' />
+          />
         </div>
         <button className='btn'>Add Transaction</button>
       </form>
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Transaction } from './Transaction';
+import { GlobalContext } from '../context/GlobalState';
 
 export const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
   return (
     <>
       <h2>History</h2>
       <ul id='list' className='list'>
-        <li className='minus'>
-          Cash <span>-£200</span>
-          <button className='delete-btn'>x</button>
-        </li>
+        {transactions.map(transaction => (
+          <Transaction transaction={transaction} key={transaction.id} />
+        ))}
       </ul>
     </>
   );
